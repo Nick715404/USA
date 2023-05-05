e96f5b5ab37d5b1b4c64c1770a48990509c11638
nicklllson <nicklllson.dev@gmail.com>
2023-05-05 13:23:35 +0500

    Добавил слайдер и подгрузку видео

--- js/main.js
+++ js/main.js
@@ -1,10 +1,54 @@
 'use strict'
 
-  const burgerBtn = document.querySelector('.header__burger');
+window.addEventListener('DOMContentLoaded', () => {
+  var video = document.querySelectorAll('.video');
 
-  document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('burger').addEventListener('click', () => {
-        document.querySelector('header').classList.toggle('open');
-    });
+  video.forEach((video) => {
 
+    video.addEventListener('click', () => {
+
+      if (video.classList.contains('ready')) {
+        return;
+      }
+
+      video.classList.add('ready')
+
+      var src = video.dataset.src;
+
+      video.insertAdjacentHTML('afterbegin', '<iframe class="video__frame" src="' + src + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+    })
+  })
+});
+
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 6,
+  // freeMode: true,
+  spaceBetween: 10,
+  grabCursor: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    580: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 6,
+    }
+  }
+});
+
+const burgerBtn = document.querySelector('.header__burger');
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('burger').addEventListener('click', () => {
+    document.querySelector('header').classList.toggle('open');
   });
+
+});
